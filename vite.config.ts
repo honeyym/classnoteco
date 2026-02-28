2aabcceeb87d5c07a6262a279f56cda2bcbb3015
katekuehl <katekuehl@gmail.com>
2026-02-28 12:46:44 -0600

    Add esbuild configuration to Vite setup
    - Introduced esbuild settings to drop console and debugger statements during the build process, optimizing the output for production environments.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  esbuild: {
+    drop: ["console", "debugger"] as ["console", "debugger"],
+  },
   build: {
     minify: "esbuild" as const,
     sourcemap: false,
